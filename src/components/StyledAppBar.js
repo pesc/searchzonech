19e5c248df5b9282ff952e8ddc7c66a08a874953
Pascal Christen <pascal@pascalchristen.ch>
2021-02-24 20:20:02 +0100

    Update About/API/URL

--- src/components/StyledAppBar.js
+++ src/components/StyledAppBar.js
@@ -32,7 +32,6 @@ export default function HeaderBar() {
     return (
         <React.Fragment>
             <CssBaseline />
-
             <AppBar position="relative" className={classes.root}>
                 <Toolbar className={classes.toolbar}>
                     <Grid container
@@ -60,7 +59,7 @@ export default function HeaderBar() {
                                         <IconButton onClick={() => navigate("/impress")} aria-label="About searchzone.ch" color="inherit">
                                             <Info fontSize='small' />
                                         </IconButton>
-                                        <IconButton onClick={() => window.open('https://github.com/pesc/.ch-searchzone', '_blank')} aria-label="Source Code" color="inherit">
+                                        <IconButton onClick={() => window.open('https://github.com/pesc/searchzonech', '_blank')} aria-label="Source Code" color="inherit">
                                             <GitHub fontSize='small' />
                                         </IconButton>
                                         <IconButton onClick={() => navigate("/api")} aria-label="API" color="inherit">
--- src/views/Impress.js
+++ src/views/Impress.js
@@ -1,50 +1,84 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     root: {
-        maxWidth: 345,
+        minWidth: 275,
+        margin: 15,
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 5,
     },
 });
 
-export default function ImgMediaCard() {
+export default function SimpleCard() {
     const classes = useStyles();
-
     return (
-        <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                />
+        <>
+            <Card className={classes.root}>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-          </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-          </Typography>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Kontaktadresse
+                </Typography>
+                    <Typography variant="h5" component="h2">
+                        Pascal Christen
+                </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Zugerstrasse 35<br />
+                        6312 Steinhausen<br />
+                        Schweiz
+                </Typography>
                 </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
-        </Button>
-                <Button size="small" color="primary">
-                    Learn More
-        </Button>
-            </CardActions>
-        </Card>
+                <CardActions>
+                    <Button component="a" href="mailto:searchzone@pascalchristen.ch" size="small">Contact Me</Button>
+                </CardActions>
+            </Card>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Impressum
+                </Typography>
+                    <Typography variant="h5" component="h2">
+                        Haftungsausschluss
+                </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Der Autor übernimmt keinerlei Gewähr hinsichtlich der inhaltlichen Richtigkeit, Genauigkeit, Aktualität,
+                        Zuverlässigkeit und Vollständigkeit der Informationen. Haftungsansprüche gegen den Autor wegen Schäden materieller
+                        oder immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw. Nichtnutzung der veröffentlichten
+                        Informationen, durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden
+                        ausgeschlossen. Alle Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder
+                        das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung
+                        zeitweise oder endgültig einzustellen.
+                </Typography>
+                    <Typography variant="h5" component="h2">
+                        Haftung für Links
+                </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs Es wird jegliche
+                        Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene
+                        Gefahr des Nutzers oder der Nutzerin.
+                </Typography>
+                    <Typography variant="h5" component="h2">
+                        Urheberrechte
+                </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören
+                        ausschliesslich Pascal Christen oder den speziell genannten Rechtsinhabern. Für die Reproduktion jeglicher
+                        Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.
+                </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button component="a" href="https://webkoenig.ch" size="small">Quelle</Button>
+                </CardActions>
+            </Card>
+        </>
     );
 }
