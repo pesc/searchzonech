c0e17aef970fdf76c0db9f2a5b47fc9c36b3ad81
Dijan <maroben@daishen.ch>
2021-03-03 20:18:14 +0100

    Formatting and react conventions (#6)
    * add prettier, rename files and refactor some small stuff
    
    * Fix react error

---
+++ src/components/Paging.jsx
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Hidden from '@material-ui/core/Hidden'
+import Pagination from '@material-ui/lab/Pagination'
+
+const useStyles = makeStyles((theme) => ({
+    pagination: {
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+        textAlign: 'center',
+    },
+}))
+export default function PagingView({ className, current, onChange, totalPages }) {
+    const classes = useStyles()
+    return (
+        <Fragment>
+            <Hidden only={['sm', 'xs']}>
+                <Pagination
+                    color="primary"
+                    size="large"
+                    boundaryCount={1}
+                    className={classes.pagination}
+                    count={totalPages}
+                    page={current}
+                    onChange={(event, val) => onChange(val)}
+                />
+            </Hidden>
+            <Hidden only={['md', 'lg', 'xl']}>
+                <Pagination
+                    color="primary"
+                    size="small"
+                    boundaryCount={1}
+                    className={classes.pagination}
+                    count={totalPages}
+                    page={current}
+                    onChange={(event, val) => onChange(val)}
+                />
+            </Hidden>
+        </Fragment>
+    )
+}
