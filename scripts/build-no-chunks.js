feea6ca8b27031f5dc10257e8a28454778c8126c
Pascal Christen <pascal@pascalchristen.ch>
2021-03-05 11:31:53 +0100

    run prettier and npm update

--- scripts/build-no-chunks.js
+++ scripts/build-no-chunks.js
@@ -2,14 +2,14 @@
 // I am including this so that when we build, we get a single, "main.xxxx.js" file
 // in our build, which we can include in website.
 
-const rewire = require("rewire");
-const defaults = rewire("react-scripts/scripts/build.js");
-let config = defaults.__get__("config");
+const rewire = require('rewire')
+const defaults = rewire('react-scripts/scripts/build.js')
+let config = defaults.__get__('config')
 
 config.optimization.splitChunks = {
-  cacheGroups: {
-    default: false
-  }
-};
+    cacheGroups: {
+        default: false,
+    },
+}
 
-config.optimization.runtimeChunk = false;
+config.optimization.runtimeChunk = false
